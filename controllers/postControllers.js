2ce0ae2a683de915596cde0bf1c4f69c55a771dd
Shashank Naik <shashanknaik808@gmail.com>
2023-10-06 16:50:58 +0530

    Building Post Model to use MySQL connection Pool

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,2 +1,11 @@
 const Post = require("../models/Post.js");
 
+exports.getAllPosts = async (req, res, next) => {
+    try {
+        const [posts, _] = await Post.findAll();
+
+        res.status(200).json({ count: posts.length, posts });
+    } catch (error) {
+        next(error);
+    }
+};
